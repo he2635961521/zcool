a891dca90caef0391312aa8b6cef2585a5b743ee
your Name <you@example.com>
2019-10-14 12:11:21 +0800

    home页面

---
+++ src/views/home/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="page-home">
+      <!-- 一级路由页面,首页 -->
+
+    <!-- 二级路由页面坑 -->
+    <router-view></router-view>
+
+    <ul class="navbar">
+        <router-link tag="li" to="/films">
+            <i class="iconfont icon-films"></i>
+            <span>电影</span>
+        </router-link>
+        <router-link tag="li" to="/cinemas">
+            <i class="iconfont icon-cinemas"></i>
+            <span>影院</span>
+        </router-link>
+        <router-link tag="li" to="/center">
+            <i class="iconfont icon-center"></i>
+            <span>我的</span>
+        </router-link>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+// @import '../../assets/common/mixins.scss'
+
+.page-home{
+    height:100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+
+    > div:first-child {
+        flex: 1;
+        overflow-y: auto
+    }
+
+    .navbar {
+        display:flex;
+        height:50px;
+
+        li {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            height:50px;
+            text-align: center;
+            color: #797d82;
+
+            i {
+                margin-bottom: 2px;
+                font-size: 24px;
+            }
+
+            span {
+                font-size: 12px
+            }
+
+            &.router-link-active {
+                color: #ff5f16;
+            }
+        }
+    }
+
+
+}
+</style>
---
+++ src/views/home/films.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="page-home-films">
+        <div class="tabs">
+            <ul>
+                <li
+                :class="{'active': curFilmType =='nowPlaying'}"
+                @click="chgFilmType('nowPlaying')"
+                >正在热映</li>
+                <li
+                :class="{'active': curFilmType =='nowPlaying'}"
+                @click="chgFilmType('nowPlaying')"
+                >即将上映</li>
+            </ul>
+        </div>
+
+
+
+
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Films',
+
+        data () {
+            return {
+                curFilmType: 'nowPlaying' //当前的影片类型
+            }
+        },
+
+        methods : {
+            /**
+             * 切换当前的影片类型
+             */
+            chgFilmType (type) {
+                this.curFilmType = type
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+.page-home-films {
+    .tabs {
+        position: sticky;
+        top: 0;
+        z-index: 999;
+        background: #fff;
+        height:50px;
+
+        ul {
+      display: flex;
+      align-items: center;
+      height: 50px;
+
+      li {
+        flex: 1;
+        text-align: center;
+
+        &.active {
+          color: #ff5f16;
+        }
+      }
+    }
+
+        .active-line {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 2px;
+        width: 50%;
+        transition: left 0.3s;
+
+        span {
+            display: block;
+            width: 56px;
+            height: 2px;
+            margin: auto;
+            background: #ff5f16;
+        }
+      }
+    }
+}
+</style>
