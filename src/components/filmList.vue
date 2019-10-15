ddcc2a0f5bf8a63fd87c5322cbf36fff40e32436
your Name <you@example.com>
2019-10-15 16:44:38 +0800

    第三个轮播

--- src/components/filmList.vue
+++ src/components/filmList.vue
@@ -1,46 +1,178 @@
 <template>
   <div>
-    <van-swipe class="banner" :autoplay="3000" indicator-color="white">
-      <div class="lunbo">
-        <van-swipe-item>
+    <div class="swiper-container1">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/734/308.jpg" />
-        </van-swipe-item>
-        <van-swipe-item>
+        </div>
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/410/809.jpg" />
-        </van-swipe-item>
-        <van-swipe-item>
+        </div>
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/753/912.jpg" />
-        </van-swipe-item>
-        <van-swipe-item>
+        </div>
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/53.jpg" />
-        </van-swipe-item>
-        <van-swipe-item>
+        </div>
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/751/651.jpg" />
-        </van-swipe-item>
-        <van-swipe-item>
+        </div>
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/756/202.jpg" />
-        </van-swipe-item>
-        <van-swipe-item>
+        </div>
+        <div class="swiper-slide">
           <img src="http://movie.miguvideo.com/publish/i_www/image/70/747/433.jpg" />
-        </van-swipe-item>
+        </div>
       </div>
-    </van-swipe>
+      <!-- Add Pagination -->
+      <div class="swiper-pagination1"></div>
+    </div>
 
     <div class="module">
       <div class="module-name grey font-16">正在售票</div>
-      <div class="swiper-container" style="height: 100px">
+      <div class="swiper-container">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
-          <div class="swiper-slide">Slide 4</div>
-          <div class="swiper-slide">Slide 5</div>
-          <div class="swiper-slide">Slide 6</div>
-          <div class="swiper-slide">Slide 7</div>
-          <div class="swiper-slide">Slide 8</div>
-          <div class="swiper-slide">Slide 9</div>
-          <div class="swiper-slide">Slide 10</div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/851.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">五月天人生无限公司</p>
+              <p class="font-13 light-grey">400万乐迷主演</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/751/725.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">阿拉丁</p>
+              <p class="font-13 light-grey">童年神话真人版</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/747/857.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">哥斯拉2：怪兽之王</p>
+              <p class="font-13 light-grey">史诗级动作冒险巨制</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/751/726.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">龙珠超：布罗利</p>
+              <p class="font-13 light-grey">龙珠超剧场版</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/859.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">大侦探皮卡丘</p>
+              <p class="font-13 light-grey">宠物小精灵真人版</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/747/859.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">一条狗的使命2</p>
+              <p class="font-13 light-grey">原班人马打造</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/734/459.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">何以为家</p>
+              <p class="font-13 light-grey">奥斯卡提名外语片</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/735/834.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">一个母亲的复仇</p>
+              <p class="font-13 light-grey">德里黑公交案改编</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/882.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">X战警：黑凤凰</p>
+              <p class="font-13 light-grey">次代X战警谢幕表演</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/885.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">云端之上</p>
+              <p class="font-13 light-grey">小鞋子导演新作</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/886.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">大大哒</p>
+              <p class="font-13 light-grey">马来西亚国宝级导演</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/755/891.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey">真爱百分百</p>
+              <p class="font-13 light-grey">真爱不在意外表</p>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <img src="http://movie.miguvideo.com/publish/i_www/image/70/379/192.jpg" />
+            <div class="content-intro">
+              <p class="font-14 grey"></p>
+              <p class="font-13 light-grey"></p>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+
+    <div class="module">
+      <div class="module-name grey font-16">精彩活动</div>
+      <div class="swiper-container2">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="content-pic">
+              <img src="http://movie.miguvideo.com/publish/i_www/image/70/738/825.gif" />
+              <img
+                class="status"
+                src="http://movie.miguvideo.com/lovev/miguMovie/images/icon/status-keepOn.png"
+              />
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="content-pic">
+              <img src="http://movie.miguvideo.com/publish/i_www/image/70/726/812.gif" />
+              <img
+                class="status"
+                src="http://movie.miguvideo.com/lovev/miguMovie/images/icon/status-keepOn.png"
+                alt
+              />
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="content-pic">
+              <img src="http://movie.miguvideo.com/publish/i_www/image/70/496/903.gif" />
+              <img
+                class="status"
+                src="http://movie.miguvideo.com/lovev/miguMovie/images/icon/status-keepOn.png"
+                alt
+              />
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="content-pic">
+              <img src="http://movie.miguvideo.com/publish/i_www/image/70/676/884.gif" />
+              <img
+                class="status"
+                src="http://movie.miguvideo.com/lovev/miguMovie/images/icon/status-keepOn.png"
+                alt
+              />
+            </div>
+          </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination2"></div>
       </div>
     </div>
   </div>
@@ -50,20 +182,45 @@
 import Swiper from "swiper";
 import "swiper/css/swiper.min.css";
 export default {
-  data() {
-    return {
-      current: 0
-    };
-  },
-  methods: {
-    onChange(index) {
-      this.current = index;
-    }
-  },
   mounted() {
     new Swiper(".swiper-container", {
-      slidesPerView: 2,
-      spaceBetween: 30
+      slidesPerView: 3.2,
+      spaceBetween: 0
+    });
+
+    new Swiper(".swiper-container1", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      },
+      loop: true,
+      speed: 300,
+      autoplay: {
+        disableOnInteraction: false,
+        delay: 2000
+      },
+      pagination: {
+        el: ".swiper-pagination1"
+      }
+    });
+
+    new Swiper(".swiper-container2", {
+      pagination: {
+        el: ".swiper-pagination2",
+        type: "progressbar"
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      loop: true
     });
   }
 };
@@ -85,6 +242,7 @@ export default {
   width: 100%;
   height: 276px;
   // background:red;
+  position: relative;
 
   .module-name {
     height: 45px;
@@ -116,29 +274,101 @@ export default {
       width: 100%;
     }
   }
+
+  .font-14 {
+    font-size: 14px !important;
+  }
+
+  .light-grey {
+    color: #999;
+  }
+
+  .font-13 {
+    font-size: 13px !important;
+  }
 }
 
 .swiper-container {
+  .swiper-slide {
+    height: 229px;
+    font-size: 18px;
+    background: #fff;
+
+    /* Center slide text vertically */
+    // display: -webkit-box;
+    // display: -ms-flexbox;
+    // display: -webkit-flex;
+    // display: flex;
+    // -webkit-justify-content: center;
+    // justify-content: center;
+    .content-intro p {
+      line-height: 1.7em;
+      padding-left: 12px;
+      padding-right: 12px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+    img {
+      width: 100%;
+      height: 179px;
+      position: relative;
+      top: 0;
+    }
+  }
+}
+
+.swiper-container1 {
   width: 100%;
-  height: 100%;
+  padding-top: 10px;
+  padding-bottom: 10px;
+
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 300px;
+    height: 300px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+.swiper-container2 {
+  height: 140px;
+  position: relative;
+  .swiper-pagination2 {
+    width: 100%;
+    height: 2px;
+    position: absolute;
+    bottom: 0;
+    z-index: 99
+  }
 }
 .swiper-slide {
+  width: 100%;
   text-align: center;
   font-size: 18px;
   background: #fff;
 
-  /* Center slide text vertically */
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: -webkit-flex;
-  display: flex;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  align-items: center;
+  .content-pic {
+    width: 100%;
+    position: relative;
+
+    img {
+      width: 375px;
+      height: 140px;
+    }
+
+    .status {
+      width: 40px !important;
+      position: absolute;
+      right: 0;
+      top: 10px;
+      height: 18px;
+    }
+  }
 }
 </style>
