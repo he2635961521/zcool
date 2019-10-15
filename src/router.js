5a3dd5d75aca68f587e08878801ce9e2b7db123d
ass <ass@163.com>
2019-10-15 08:55:54 +0800

    aa

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Person from './views/person/person.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,11 @@ export default new Router({
       name: 'home',
       component: Home
     },
+    {
+      path: '/person',
+      name: 'person',
+      component: Person
+    }
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/person/person.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        我是person
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'person',
+  beforeRouteEnter: (to, from, next) => {
+    console.log(this)
+    console.log('进入之前')
+    next()
+  }
+}
+</script>
