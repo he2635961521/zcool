6b8770dbbc5cedb2c037ed16bb9fd7e0a89883d1
your Name <you@example.com>
2019-10-17 14:53:39 +0800

    设置小视频网站链接

--- src/components/hdh/video.vue
+++ src/components/hdh/video.vue
@@ -2,12 +2,14 @@
   <div>
     <div class="box"></div>
     <div class="image" v-for="(film,index) in film4" :key="index">
-      <a href="#">
+      <!-- <router-link> -->
+      <a :href="`http://movie.miguvideo.com/publish/i_www/resource/lovev/miguMovie/detail/trailer.jsp?contentId=${film.picList[0].REDREICT_ID}&cid=${film.picList[0].REDREICT_ID}`" >
         <div class="content-pic">
           <img :src="`http://movie.miguvideo.com/publish/${film.picList[0].imgSrc}`" />
           <span class="update-to font-14 white">{{film.name}}</span>
         </div>
       </a>
+      <!-- </router-link> -->
     </div>
   </div>
 </template>
@@ -49,25 +51,13 @@ export default {
         this.filmList = response.data;
         this.film4 = this.filmList[1].list;
         console.log(this.film4);
-
-        //if (result.status === 0) {
-        // commit('setFilmList', result.data.films)
-        // 要先仓库中 filmList 的数据，与请求得来的数据做一个拼接的操作
-        //commit("setFilmList", state.filmList.concat(result.data.films));
-        // commit('setFilmList', state.filmList.push(result.data.films))
-        // let filmList = state.filmList
-        // filmList.push(...result.data.films)
-        // commit('setFilmList', filmList)
-
-        // 执行传递过来的 payload.callback 这个回调函数
-        // payload.callback && payload.callback()
-
-        // if (payload.callback) {
-        //   payload.callback();
-        // }
-        //}
       });
   }
+  // methods : {
+  //   fn1() {
+  //     return  window.location.href='http://www.baidu.com'
+  //   }
+  // }
 };
 </script>
 
